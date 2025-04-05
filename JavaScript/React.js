a0195e131d676f5b0fed5a6e15c89fd760957378
PennasenKake <121464958+PennasenKake@users.noreply.github.com>
2025-04-05 23:35:10 +0300

    react js lisää

--- JavaScript/React.js
+++ JavaScript/React.js
@@ -102,6 +102,117 @@ const myRef = useRef(initialValue);
 
 
 
+// REACT - Event Handling yhdistettynä useState-hookiin
+
+
+/*
+Tapahtumien käsittely
+Tapa määritellä funktioita, jotka vastaavat tiettyihin käyttäjän vuorovaikutuksiin, 
+kuten klikkauksiin, lomakkeen lähettämiseen ja syötteiden muutoksiin. 
+*/
+const handleClick = () => {
+    // Käsittele klikkaustapahtuma
+    console.log("Nappia klikattu!");
+};
+  
+
+
+
+/*
+useState tapahtumakäsittelyssä
+Integroi tapahtumakäsittelijät useState-hookiin, 
+jolloin voit päivittää komponentin tilaa dynaamisesti käyttäjän syötteen perusteella. 
+*/
+import React, { useState } from "react";
+
+const ExampleComponent = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={handleChange} 
+        placeholder="Kirjoita jotain..."
+      />
+      <p>Syötit: {inputValue}</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
+
+
+
+
+/*
+    Selitys:
+    handleClick on yksinkertainen tapahtumakäsittelijä, joka reagoi klikkaustapahtumaan.
+
+    handleChange päivittyy automaattisesti, kun käyttäjä muuttaa syötteitä.
+
+    useState mahdollistaa tilan (state) hallinnan 
+    ja sen dynaamisen muutoksen, esimerkiksi tekstikentän arvojen muuttuessa.
+*/
+
+
+
+// React - Conditional Rendering (Ehdollinen renderöinti)
+
+/*
+Ternary Operator
+Kuvaus: Käytetään boolean-ehdon perusteella näyttämään joko yksi tai toinen komponentti.
+Esimerkki:
+*/
+return isLogged ? <UserGreeting /> : <GuestGreeting />;
+
+
+/*
+Short Circuit Evaluation
+Kuvaus: Käytetään boolean-ehdon perusteella näyttämään komponentti vain, jos ehto on tosi.
+Esimerkki:
+*/
+return isLoggedIn && <UserGreeting />;
+
+
+
+// React - Lists and Keys
+
+
+
+/*
+Rendering Lists
+Listojen avulla voit helposti renderöidä useita komponentteja tehokkaasti.
+Esimerkki: 
+*/
+const myList = [1, 2, 3];
+
+const listItems = myList.map((item) => (
+  <li key={item}>{item}</li>
+));
+/*
+map-metodi: Luo uuden taulukon komponentteja iteroinnin perusteella.
+Key-prop: Jokaisella listakomponentilla tulisi 
+olla ainutlaatuinen key-prop nopeuttaaksesi renderöintiä ja estääksesi virheitä. 
+*/
+
+
+/*
+Fragment Shorthand
+Käytä fragmentteja välttääksesi turhaa HTML-elementtien pesittämistä.
+Esimerkki: 
+*/
+return <>{listItems}</>;
+// Fragmentit: <></>-lyhennemuoto toimii kuten <React.Fragment>.
+
+
 /* */
 /* */
+/* */
+
 
